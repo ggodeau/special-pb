d036b4c308115b1d4a96ed469573dd0362de7f6f
ggodeau <ggodeau3@gatech.edu>
2017-09-25 15:55:51 +0200

    Change in path and add of cp_files

--- scripts/constants.py
+++ scripts/constants.py
@@ -2,19 +2,19 @@
 HOME_DIR = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/'
 
 # Path to SALICON raw data
-pathToImages = '/home/users/jpang/salicon_data/images'
-pathToMaps = '/home/users/jpang/salicon_data/saliency'
-pathToFixationMaps = '/home/users/jpang/salicon_data/fixation'
+pathToImages = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/images'
+pathToMaps = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/maps'
+pathToFixationMaps = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/fixations'
 
 # Path to processed data
-pathOutputImages = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/images320x240'
-pathOutputMaps = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/saliency320x240'
-pathToPickle = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/320x240'
+pathOutputImages = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/images320x240'
+pathOutputMaps = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/saliency320x240'
+pathToPickle = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/320x240'
 
 # Path to pickles which contains processed data
-TRAIN_DATA_DIR = '/home/users/jpang/scratch-local/salicon_data/320x240/fix_trainData.pickle'
-VAL_DATA_DIR = '/home/users/jpang/scratch-local/salicon_data/320x240/fix_validationData.pickle'
-TEST_DATA_DIR = '/home/users/jpang/scratch-local/salicon_data/256x192/testData.pickle'
+TRAIN_DATA_DIR = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/320x240/fix_trainData.pickle'
+VAL_DATA_DIR = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/320x240/fix_validationData.pickle'
+TEST_DATA_DIR = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/dataSet/256x192/testData.pickle'
 
 # Path to vgg16 pre-trained weights
 PATH_TO_VGG16_WEIGHTS = '/home/GTL/ggodeau/special_pb/saliency-salgan-2017/vgg16.pkl'
